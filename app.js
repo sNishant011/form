3777992618e06ac3ad089b5681f5de7a0376f689
snishant011 <shresthanishant011@gmail.com>
2021-09-14 09:50:51 +0545

    password validation remaining

--- app.js
+++ app.js
@@ -3,22 +3,16 @@ const toggleDoB = () => {
   dateField.setAttribute('type', 'date')
 }
 const email_db = ['shresthanishant011@gmail.com']
-var name = document.getElementById('name')
-var email = document.getElementById('email')
-var uname = document.getElementById('uname')
-var dob = document.getElementById('dob')
-var dob = document.getElementById('dob')
-var password1 = document.getElementById('password1')
-var password2 = document.getElementById('password2')
-var form = document.getElementById('sign-up')
+const user_db = ['srestn011', 'nishant']
 var isNameValid = false
 var isEmailValid = false
+var isUsernameValid = false
+var isdobValid = false
 // validates email
 const emailValidator = (email) => {
   const email_warner = document.getElementById('email-warning')
   if (email !== '') {
     if (email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-      console.log('Valid format')
       if (email_db.includes(email)) {
         isEmailValid = false
         email_warner.innerHTML = 'arkako email nalekhna bhai.'
@@ -30,17 +24,61 @@ const emailValidator = (email) => {
       email_warner.innerHTML = 'Email kasto hunxa tha xaina talai?'
     }
   }
-  console.log(isEmailValid)
 }
 
 const nameValidator = (len) => {
   const name_warner = document.getElementById('name-warning')
 
   if (len > 32) {
-    name_warner.innerHTML = 'kati lamo name ho... agadi rw paxadi ko matra lekh'
+    name_warner.innerHTML = 'kati lamo name agadi rw paxadi ko matra lekh'
+    isNameValid = false
+  } else if (len < 2) {
+    name_warner.innerHTML = 'name nai xaina ki k ho'
     isNameValid = false
   } else {
     name_warner.innerHTML = ''
     isNameValid = true
+  }
+}
+
+const usernameValidator = (username) => {
+  const username_warner = document.getElementById('username-warning')
+  if (username.match(/[-!$%^&*()_+|~=`\\#{}\[\]:";'<>?,.\/]/)) {
+    isUsernameValid = false
+    username_warner.innerHTML = 'Jpt name narakhna hey bhagwan!!'
+    if (username.length > 20) {
+      isUsernameValid = false
+      username_warner.innerHTML = 'Jpt lamo name narakhna hey bhagwan!!'
+    }
+  } else {
+    if (username.length > 20) {
+      isUsernameValid = false
+      username_warner.innerHTML =
+        'ajhai lamo rakhna.. database bharini tw mero ho'
+    } else if (user_db.includes(username)) {
+      isUsernameValid = false
+
+      username_warner.innerHTML = 'ali unique rakhna.. yo name pailai xa'
+    } else {
+      isUsernameValid = true
+      username_warner.innerHTML = ''
+    }
+  }
+}
+
+const dobValidator = (dob) => {
+  const dob_warner = document.getElementById('dob-warning')
+
+  const age = Date.now() - Date.parse(dob)
+  if (age >= 567993600000) {
+    isdobValid = true
+    dob_warner.innerHTML = ''
+  } else {
+    isdobValid = false
+    if (Date.parse(dob) > Date.now()) {
+      dob_warner.innerHTML = 'Janminai baki raixas'
+    } else {
+      dob_warner.innerHTML = 'bacchai xas'
+    }
   }
 }
